fa4d340c8403bc425245ade5af2d1f96a0bcff6b
giraldiego <giraldiego@gmail.com>
2021-11-15 10:36:06 -0500

    Add some boilerplate code for some routes

--- routes/subscribers.js
+++ routes/subscribers.js
@@ -2,3 +2,18 @@ const express = require('express');
 const router = express.Router();
 
 module.exports = router;
+
+// Get all
+router.get('/', (req, res) => {
+    res.send('Show list of items')
+});
+
+// Get one
+router.get('/:id', (req, res) => {
+    res.send('You are asking for details about ' + req.params.id);
+});
+
+// Create one
+router.post('/', (req, res) => {
+    res.send('You want to create something');
+});
