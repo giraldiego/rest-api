81a30438731661f7b90a3015ea28a73edfb91b44
giraldiego <giraldiego@gmail.com>
2021-11-15 14:13:23 -0500

    Add Subscriber model

---
+++ models/subscriber.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+
+const { Schema } = mongoose;
+
+const subscriberSchema = new Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    subscriberToChannel: {
+        type: String,
+        required: true
+    },
+    subscribeDate: {
+        type: Date,
+        required: true,
+        default: Date.now()
+    }
+});
+
+// Export the model that allow us to interact with the DB
+module.exports = mongoose.model('Subscriber', subscriberSchema)
